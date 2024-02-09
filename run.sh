cc1fe1b4442488e0078ab7fde9198bbab8b4409a
paradox8599 <paradox8599@outlook.com>
2024-02-09 11:08:22 +1100

    style: run script variable in quotes

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ while true; do
 	echo "[backup] pulling db to $fn"
 	pg_dump "$DATABASE_URL" >"$fn"
 	echo "[backup] uploading backup"
-	aws s3 cp $fn "s3://$AWS_BUCKET/sql/$fn"
+	aws s3 cp "$fn" "s3://$AWS_BUCKET/sql/$fn"
 	echo "[backup] clean up"
 	rm -f "$fn"
 	echo "[backup] done, waiting for next backup"
