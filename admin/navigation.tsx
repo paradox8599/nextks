5ed03be1bbe25dc60c8355b6fbc473f5bc7cd5df
paradox8599 <paradox8599@outlook.com>
2024-03-04 16:06:33 +1100

    feat: added custom navigation sidebar

---
+++ admin/navigation.tsx
@@ -0,0 +1,15 @@
+import {
+  NavigationContainer,
+  ListNavItems,
+  NavItem,
+} from "@keystone-6/core/admin-ui/components";
+import type { NavigationProps } from "@keystone-6/core/admin-ui/components";
+
+export function Navigation({ lists, authenticatedItem }: NavigationProps) {
+  return (
+    <NavigationContainer authenticatedItem={authenticatedItem}>
+      <NavItem href="/">Dashboard</NavItem>
+      <ListNavItems lists={lists} />
+    </NavigationContainer>
+  );
+}
