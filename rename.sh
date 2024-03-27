c0829ec82a205fb440dc4253c8aad7e79fb88bac
paradox8599 <paradox8599@outlook.com>
2024-03-27 16:57:18 +1100

    chore: script for renaming the app

---
+++ rename.sh
@@ -0,0 +1,6 @@
+name=$1
+sed -i '' "s/nextks/$name/g" compose.yaml
+sed -i '' "s/nextks/$name/g" fly.toml
+sed -i '' "s/nextks/$name/g" package.json
+sed -i '' "s/nextks/$name/g" package-lock.json
+sed -i '' "s/nextks/$name/g" README.md
