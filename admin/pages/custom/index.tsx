540ea079e13659be75a1a6691bcbadfc789f7aa1
paradox8599 <paradox8599@outlook.com>
2024-10-16 12:31:12 +1100

    fix: make iframe take full height and width

--- admin/pages/custom/index.tsx
+++ admin/pages/custom/index.tsx
@@ -3,7 +3,7 @@ import AdminPageLayout from "../../layout-admin";
 export default function Page() {
   return (
     <AdminPageLayout header="Custom">
-      <iframe src="/" />
+      <iframe src="/" width="100%" height="100%" />
     </AdminPageLayout>
   );
 }
