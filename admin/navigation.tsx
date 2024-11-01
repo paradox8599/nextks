94342e514b101d02dbd7269a11c0900d183fa19e
paradox8599 <paradox8599@outlook.com>
2024-11-01 15:47:22 +1100

    feat: set admin page title

--- admin/navigation.tsx
+++ admin/navigation.tsx
@@ -4,8 +4,12 @@ import {
   ListNavItems,
 } from "@keystone-6/core/admin-ui/components";
 import type { NavigationProps } from "@keystone-6/core/admin-ui/components";
+import React from "react";
 
 export function Navigation({ authenticatedItem, lists }: NavigationProps) {
+  React.useEffect(() => {
+    document.title = "NextKS";
+  }, []);
   return (
     <NavigationContainer authenticatedItem={authenticatedItem}>
       <NavItem href="/">Dashboard</NavItem>
